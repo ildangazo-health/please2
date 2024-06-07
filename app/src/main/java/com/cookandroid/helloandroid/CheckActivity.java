4fdb337757215fb14da0805cc2fa93498c6a20d7
rkfka <rkfka4291@naver.com>
2024-06-07 09:43:12 +0900

    commit

---
+++ app/src/main/java/com/cookandroid/helloandroid/CheckActivity.java
@@ -0,0 +1,24 @@
+package com.cookandroid.helloandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CheckActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checklist);
+
+        Button loginButton = findViewById(R.id.button_submit);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckActivity.this,ListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/cookandroid/helloandroid/ListActivity.java
@@ -0,0 +1,12 @@
+package com.cookandroid.helloandroid;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_result);
+    }
+}
